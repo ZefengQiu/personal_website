b0cc1c5b46105f6605d050497f0fb2933b125974
Will Qiu <will.qiu@xe.com>
2019-01-13 22:11:54 -0500

    save on sunday

---
+++ server/services/landscapesService.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+class LandscapesService {
+    constructor(datafile) {
+        this.datafile = datafile;
+    }
+
+    async getData() {
+        const data = await readFile(this.datafile, 'utf8');
+        if (!data) return [];
+        return JSON.parse(data).landscapes;
+    }
+}
+
+module.exports = LandscapesService;
