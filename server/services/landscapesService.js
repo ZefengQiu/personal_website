03be938590de5614389f533b8ea53cda1521c233
ZefengQiu <z9qiu@uwaterloo.ca>
2019-01-21 19:28:59 -0500

    first commit

--- server/services/landscapesService.js
+++ server/services/landscapesService.js
@@ -3,16 +3,18 @@ const util = require('util');
 
 const readFile = util.promisify(fs.readFile);
 
-class LandscapesService {
+module.exports = class LandscapesService {
     constructor(datafile) {
         this.datafile = datafile;
-    }
+    };
 
     async getData() {
         const data = await readFile(this.datafile, 'utf8');
         if (!data) return [];
         return JSON.parse(data).landscapes;
-    }
-}
+    };
 
-module.exports = LandscapesService;
+    async getArtworksForAlbum() {
+        
+    }
+};
